cf445b72206a9c5d6de40bceea6926653e8301df
Verceles <qjgdverceles@tip.edu.ph>
2023-02-20 10:41:43 +0000

    Add Class_Record

---
+++ Class_Record.py
@@ -0,0 +1,12 @@
+import Student
+import Grades
+
+p1=Student.Student("Jubileo Verceles", 22)
+p2=Grades.Grades("CPE028", "3 Units", 1)
+print (p1)
+print (p2)
+print ("Name:", p1.name)
+print ("Age:",p1.age)
+print ("Course Code:", p2.code)
+print ("Course Units:", p2.units)
+print ("Course Grade:", p2.grades)
