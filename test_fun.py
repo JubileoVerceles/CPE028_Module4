67f220b779b328cc33e8e6f60b9e76f95ccfa7a2
Verceles <qjgdverceles@tip.edu.ph>
2023-02-22 11:33:16 +0000

    Add testfun

---
+++ test_fun.py
@@ -0,0 +1,8 @@
+import unittest 
+def fun(x):
+    return x+1
+class MyTest(unittest.TestCase):
+    def test (self):
+        self.assertEqual(fun(4),8)
+if __name__=='__main__':
+    unittest.main()
